e035efadedf15390e9e953121252c263f4d8181e
Joel <joeljacobs95@gmail.com>
2021-06-16 12:54:15 -0400

    route functionality done

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const express = require("express")
 const ExpressError = require("./expressError")
 
 const userRoutes = require("./userRoutes")
+const items = require("./fakeDb.js")
 
 const app = express()
 
 app.use(express.json())
 
-app.use('/users', userRoutes)
+app.use('/items', userRoutes)
+app.use(express.json())
+
 
 app.listen(3000, function () {
     console.log('App on port 3000')
 })
-
--- userRoutes.js
+++ userRoutes.js
@@ -1,19 +1,44 @@
 const express = require('express')
 const router = new express.Router()
 
-const USERS = [{ id: 1, user1: 'joelserboi' }, { id: 2, user2: 'joelserboiser' }]
+class Item {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+
+        items.push(this);
+    }
+}
 
 router.get('/', (req, res) => {
-    res.json({ users: USERS })
+    res.json({ items: items })
 })
 
-router.get('/:id', (req, res) => {
-    const user = USERS.find(u => u.id === +req.params.id)
-    req.params.id
-    res.json({ user })
+router.post('/', (req, res) => {
+    const newItem = new Item(req.body.name, req.body.price)
+    console.log(newItem)
+    return res.json({ added: newItem })
 })
 
 
+router.get('/:name', (req, res) => {
+    const item = items.find(u => u.name === req.params.name)
+
+    res.json({ item })
+})
+
+router.patch('/:name', (req, res) => {
+    const item = items.find(u => u.name === req.params.name)
+    item.name = req.body.name
+    item.price = req.body.price
+    res.json({ updated: item })
+})
+
+router.delete('/:name', (req, res) => {
+    const item = items.find(u => u.name === req.params.name)
+    const deleteIndex = items.indexOf(item)
+    items.splice(deleteIndex, 1)
+    res.json({ message: "DELETED" })
+})
 
 module.exports = router
-
